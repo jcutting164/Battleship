569f06b2c1bf59a210cca9ba86e40400092da80a
Josh Cutting <jcutting164@gmail.com>
2020-01-28 14:44:56 -0500

    ships can move
    so many problems so many problems so many problems so many problems so many problems so many problems so many problems so many problems  so many problems so many problems so many problems so many problems so many problems so many problems so many problems so many problems 
    halp

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,5 @@
 import java.awt.Canvas;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
@@ -26,7 +27,7 @@ public class Game extends Canvas implements Runnable{
 
 
     // height is 960
-    public static final float WIDTH = 1200, HEIGHT = 800;
+    public static final float WIDTH = 1200, HEIGHT = 820;
 
     //temp
 
@@ -35,11 +36,51 @@ public class Game extends Canvas implements Runnable{
     
     private KeyInput keyInput;
     private MouseInput mouseInput;
-
-    private String[] STATES = {"Menu", "Game"};
+// STATES ARE HEREEEEEEEE
+    private String[] STATES = {"Menu", "Game","Setup"};
     private String currentState="Menu";
     static Textures tex;
     private boolean startSelect=false;
+    
+    // will be used to highlight which ship is being selected
+    // NEED TO IMPLEMENT SYSTEM TO TRACK WHICH SQUARE IS SELECTED (LIKE CHECKERS BUT ACCOMODATE FOR LABELS (A-J) (1-10)
+    private String currentSelection="";
+    
+    
+    private String alpha = "ABCDEFGHIJ";
+    
+    private String boardP1[][] = {
+            //board of player one where the ships are being put
+    		/* EACH SHIP WILL BE A COLOR AT THE MOMENT UNTIL GRAPHICS ARE ADDED TO FILL SPACE
+    		 C = Carrier / RED
+    		 B = Battleship / ORANGE
+    		 R = Cruiser / YELLOW
+    		 S = Submarine / GREEN
+    		 D = Destroyer / BLUE
+    		 */
+            {"C", "C", "C", "C", "C", " ", " ", " "," "," " },
+            {"B", "B", "B", "B", " ", " ", " ", " "," "," " },
+            {"R", "R", "R", " ", " ", " ", " ", " "," "," " },
+            {"S", "S", "S", " ", " ", " ", " ", " "," "," " },
+            {"D", "D", " ", " ", " ", " ", " ", " "," "," " },
+            {" ", " ", " ", " ", " ", " ", " ", " "," "," " },
+            {" ", " ", " ", " ", " ", " ", " ", " "," "," " },
+            {" ", " ", " ", " ", " ", " ", " ", " "," "," " },
+            {" ", " ", " ", " ", " ", " ", " ", " "," "," " },
+            {" ", " ", " ", " ", " ", " ", " ", " "," "," " }
+
+
+    };
+    
+    
+    private HashMap<String,Integer> shipHash= new HashMap<>();
+    
+    
+    
+    // Coordinate arraylist of all selected pieces
+    private ArrayList<ArrayList<Integer>> selectedCoords=new ArrayList<>();
+    
+    
     
     
     
@@ -48,15 +89,24 @@ public class Game extends Canvas implements Runnable{
     private Window window;
 
     public Game() {
-    	
+       shipHash.put("C",5);
+       shipHash.put("B",4);
+       shipHash.put("R",3);
+       shipHash.put("S",3);
+       shipHash.put("D",2);
+       
+       
 
 
-       keyInput = new KeyInput();
+
+       keyInput = new KeyInput(this);
 
         this.addKeyListener(keyInput);
         
         mouseInput=new MouseInput(this);
         this.addMouseListener(mouseInput);        
+        
+        this.addMouseMotionListener(mouseInput);
         
 /*
         mouseInput= new MouseInput(this);
@@ -120,8 +170,12 @@ public class Game extends Canvas implements Runnable{
     private void tick() {
         try{
             
-           
-
+           System.out.println(mouseInput.getSetupColumn());
+           System.out.println(mouseInput.getSetupRow());
+           System.out.println("current selected: "+mouseInput.getCurrentSelected());
+           for(int i = 0; i<selectedCoords.size(); i++){
+        	   System.out.println(selectedCoords.get(i));
+           }
 
 
 
@@ -165,10 +219,92 @@ public class Game extends Canvas implements Runnable{
         		g.setColor(Color.red);
         		g.fillRect(860, 100, 256, 96);
         	}
+        	g.setColor(Color.black);
         	
         	
         	g.setFont(new Font("Serif", Font.BOLD, 45));
         	g.drawString("Start",932, 160);
+        	
+        	
+        }else if(currentState.equals("Setup")){
+        	// displaying the grid (A-J, 1-10)
+        	g.setColor(Color.blue);
+        	g.fillRect(0, 0, (int)WIDTH, (int)HEIGHT);
+        	
+        	
+        	g.setColor(Color.gray);
+        	g.fillRect(1,1,71,71);
+        	g.setColor(Color.black);
+        	g.drawRect(0, 0, 72, 72);
+        	for(int j = 0; j<11; j++){
+        		for(int i = 0; i<11; i++){
+            		g.setColor(Color.gray);
+                	g.fillRect(i*72+1,1+(72*j),71,71);
+                	g.setColor(Color.black);
+                	g.drawRect(0+(72*i), 0+(72*j), 72, 72);
+            	}
+        	}
+        	
+        	// putting grid letters and numbers
+        	// LETTERS:
+        	g.setFont(new Font("Serif", Font.BOLD, 20));
+        	for(int i = 0; i<10; i++){
+        		if(i<5){
+        			g.drawString(alpha.substring(i,i+1), (i*75)+90, 30);
+        			g.drawString(""+(i+1), 30, (i*75)+105);
+
+
+        		}
+        		else{
+        			g.drawString(alpha.substring(i,i+1), (i*75)+80, 30);
+        			g.drawString(""+(i+1), 30, (i*75)+90);
+
+
+        		}
+
+        	}
+        	// DISPLAYING SHIPS: SYMBOLS FOR NOW (will be used for the double array boards
+        	
+        	// COLORS FOR NOW
+        	// displaying the actual boards
+
+        	
+        	/* EACH SHIP WILL BE A COLOR AT THE MOMENT UNTIL GRAPHICS ARE ADDED TO FILL SPACE
+		   		 C = Carrier / RED
+		   		 B = Battleship / ORANGE
+		   		 R = Cruiser / YELLOW
+		   		 S = Submarine / GREEN
+		   		 D = Destroyer / BLUE
+		   		 */
+        	
+        	
+        	for(int i = 0; i<boardP1.length; i++){
+        		for(int j = 0; j<boardP1[0].length; j++){
+        			if(boardP1[j][i].contains("$")){
+        				g.setColor(Color.cyan);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}else if(boardP1[j][i].contains("C")){
+        				g.setColor(Color.red);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}else if(boardP1[j][i].contains("B")){
+        				g.setColor(Color.orange);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}else if(boardP1[j][i].contains("R")){
+        				g.setColor(Color.yellow);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}else if(boardP1[j][i].contains("S")){
+        				g.setColor(Color.green);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}else if(boardP1[j][i].contains("D")){
+        				g.setColor(Color.blue);
+        				g.fillRect((72)+   +i*72+1,(72)+   1+(72*j),71,71);
+        			}
+        		}
+        	}
+        	
+        	
+        	
+        	
         	
         	
         }
@@ -236,6 +372,158 @@ public class Game extends Canvas implements Runnable{
 	public void setStartSelect(boolean startSelect) {
 		this.startSelect = startSelect;
 	}
+	
+	
+	
+	
+
+	public String[][] getBoardP1() {
+		return boardP1;
+	}
+
+	public void setBoardP1(String[][] boardP1) {
+		this.boardP1 = boardP1;
+	}
+	
+	
+	public void selectAll(String shipType, String[][] board){
+		for(int i = 0; i<board.length; i++){
+			for(int j = 0; j<board[0].length; j++){
+				if(board[i][j].contains(shipType)){
+					board[i][j]+="$";
+					
+					ArrayList<Integer> tempCoords=new ArrayList<>();
+	        		tempCoords.add(i); tempCoords.add(j);
+	        		selectedCoords.add(tempCoords);					
+				
+				}
+			}
+		}
+	}
+	public void removeSelection(String[][] board){
+		for(int i = 0; i<board.length; i++){
+			for(int j = 0; j<board[0].length; j++){
+				if(board[i][j].contains("$")){
+					String temp = board[i][j];
+					String giveback="";
+					for(int loop = 0; loop<temp.length(); loop++){
+						if(!temp.substring(loop,loop+1).contains("$")){
+							giveback+=temp.substring(loop,loop+1);
+						}
+					}
+					board[i][j]=giveback;
+				}
+			}
+		}
+	}
+	// removes a character out of a 2d array based on strings
+	public void removeChar(String[][] board, String removal){
+		for(int i = 0; i<board.length; i++){
+			for(int j = 0; j<board[0].length; j++){
+				if(board[i][j].contains(removal)){
+					String temp = board[i][j];
+					String giveback="";
+					for(int loop = 0; loop<temp.length(); loop++){
+						if(!temp.substring(loop,loop+1).contains("$")){
+							giveback+=temp.substring(loop,loop+1);
+						}
+					}
+					board[i][j]=giveback;
+				}
+			}
+		}
+	}
+	// removes a character out of a singular string 
+	public void removeChar(String start, String removal){
+		String end="";
+		for(int i = 0; i<start.length(); i++){
+			if(!(start.substring(i,i+1).equals(removal))){
+				end+=start.substring(i,i+1);
+			}
+			
+		}
+	}
+	
+	// removes a character out of a singular string AND will stop after the first time it finds one
+	
+	public String removeCharSingle(String start, String removal){
+		String end="";
+		for(int i = 0; i<start.length(); i++){
+			if(!(start.substring(i,i+1).equals(removal))){
+				end+=start.substring(i,i+1);
+			}else{
+				break;
+			}
+			
+		}
+		return end;
+	}
+	
+	
+	public KeyInput getKeyInput() {
+		return keyInput;
+	}
+
+	public void setKeyInput(KeyInput keyInput) {
+		this.keyInput = keyInput;
+	}
+
+	public MouseInput getMouseInput() {
+		return mouseInput;
+	}
+
+	public void setMouseInput(MouseInput mouseInput) {
+		this.mouseInput = mouseInput;
+	}
+
+	public String getCurrentSelection() {
+		return currentSelection;
+	}
+
+	public void setCurrentSelection(String currentSelection) {
+		this.currentSelection = currentSelection;
+	}
+	
+
+	public ArrayList<ArrayList<Integer>> getSelectedCoords() {
+		return selectedCoords;
+	}
+
+	public void setSelectedCoords(ArrayList<ArrayList<Integer>> selectedCoords) {
+		this.selectedCoords = selectedCoords;
+	}
+	
+	
+
+	public HashMap<String, Integer> getShipHash() {
+		return shipHash;
+	}
+
+	public void setShipHash(HashMap<String, Integer> shipHash) {
+		this.shipHash = shipHash;
+	}
+	/* EACH SHIP WILL BE A COLOR AT THE MOMENT UNTIL GRAPHICS ARE ADDED TO FILL SPACE
+	 C = Carrier / RED
+	 B = Battleship / ORANGE
+	 R = Cruiser / YELLOW
+	 S = Submarine / GREEN
+	 D = Destroyer / BLUE
+	 */
+	public boolean listContains(ArrayList<ArrayList<Integer>> list){
+		boolean result=false;
+		for(int i = 0; i<list.size(); i++){
+			String a = boardP1[list.get(i).get(0)][list.get(i).get(1)];
+			if(! (a.contains("C") || a.contains("B") || a.contains("R") || a.contains("S") || a.contains("D"))){
+				result = true;
+				break;
+			}
+		}
+		return result;
+	}
+	
+	
+	
+	
 
 	public static void main(String[] args) {
         new Game();
--- src/KeyInput.java
+++ src/KeyInput.java
@@ -2,15 +2,16 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 
 public class KeyInput extends KeyAdapter implements Serializable {
 
 
+	private Game game;
 
-
-    public KeyInput() {
-
+    public KeyInput(Game game) {
+    	this.game=game;
     }
 
 
@@ -20,8 +21,156 @@ public class KeyInput extends KeyAdapter implements Serializable {
 
         if(key==KeyEvent.VK_ESCAPE)
             System.exit(0);
+        
+        
+        // setup stuff
+        
+        if(game.getCurrentState().equals("Setup") && !(game.getMouseInput().getCurrentSelected().equals(" "))){
+        	// UDLR
+        	
+        	/* KEY
+        	 * UP: -y1
+        	 * DOWN: +y1
+        	 * LEFT: -x1
+        	 * RIGHT: x+1
+        	 */
 
+        	
+        	
+        	if(key==KeyEvent.VK_UP){
+        		System.out.println("up");
+        		
+        		// getting the movement coords to check if available before actually movement
+        		
 
+        		
+
+        			ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+            		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+            			
+                		// clears what is currently selected
+
+            			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]="";
+            			
+            			// modifies the current selected coords
+            			ArrayList<Integer> temp2=new ArrayList<Integer>();
+            			// these lines decide the movement
+            			temp2.add(game.getSelectedCoords().get(i).get(0)-1);
+            			temp2.add(game.getSelectedCoords().get(i).get(1));
+            			temp.add(temp2);
+            		}
+            		
+            		// readds the temp list to the real list that game is using
+            		game.setSelectedCoords(temp);
+            		
+            	 		
+            		// readds the ships to the board
+            		
+            		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+            			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]+=("$"+game.getMouseInput().getCurrentSelected());
+            		}
+        		
+        		
+        		
+        		// end
+        		
+        		
+
+        	}else if(key==KeyEvent.VK_DOWN){
+        		System.out.println("down");
+        		
+        		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			
+            		// clears what is currently selected
+
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]="";
+        			
+        			// modifies the current selected coords
+        			ArrayList<Integer> temp2=new ArrayList<Integer>();
+        			// these lines decide the movement
+        			temp2.add(game.getSelectedCoords().get(i).get(0)+1);
+        			temp2.add(game.getSelectedCoords().get(i).get(1));
+        			temp.add(temp2);
+        		}
+        		
+        		// readds the temp list to the real list that game is using
+        		game.setSelectedCoords(temp);
+        		
+        	 		
+        		// readds the ships to the board
+        		
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]+=("$"+game.getMouseInput().getCurrentSelected());
+        		}
+
+        	}else if(key==KeyEvent.VK_LEFT){
+        		System.out.println("left");
+        		
+        		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			
+            		// clears what is currently selected
+
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]="";
+        			
+        			// modifies the current selected coords
+        			ArrayList<Integer> temp2=new ArrayList<Integer>();
+        			// these lines decide the movement
+        			temp2.add(game.getSelectedCoords().get(i).get(0));
+        			temp2.add(game.getSelectedCoords().get(i).get(1) - 1);
+        			temp.add(temp2);
+        		}
+        		
+        		// readds the temp list to the real list that game is using
+        		game.setSelectedCoords(temp);
+        		
+        	 		
+        		// readds the ships to the board
+        		
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]+=("$"+game.getMouseInput().getCurrentSelected());
+        		}
+        		
+
+        	}else if(key==KeyEvent.VK_RIGHT){
+        		System.out.println("right");
+        		System.out.println(game.getCurrentSelection());
+        		ArrayList<ArrayList<Integer>> temp = new ArrayList<ArrayList<Integer>>();
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			
+            		// clears what is currently selected
+
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]="";
+        			
+        			// modifies the current selected coords
+        			ArrayList<Integer> temp2=new ArrayList<Integer>();
+        			// these lines decide the movement
+        			temp2.add(game.getSelectedCoords().get(i).get(0));
+        			temp2.add(game.getSelectedCoords().get(i).get(1) + 1);
+        			temp.add(temp2);
+        		}
+        		
+        		// readds the temp list to the real list that game is using
+        		game.setSelectedCoords(temp);
+        		
+        	 		
+        		// readds the ships to the board
+        		
+        		for(int i = 0; i<game.getSelectedCoords().size(); i++){
+        			System.out.println("jajaja "+"$"+game.getCurrentSelection());
+        			System.out.println(game.getCurrentSelection());
+        			game.getBoardP1()[game.getSelectedCoords().get(i).get(0)][game.getSelectedCoords().get(i).get(1)]+=("$"+game.getMouseInput().getCurrentSelected());
+        		}
+        		
+        		
+        	}
+        	
+        	
+        }
+        
+        
+        
     }
 
 
